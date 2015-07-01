1e97fda9ac570bd42030f971e03d94e5fb84d4d2
Breese <breese@specialized-computers.com>
2015-07-01 12:32:16 -0400

    updates to file set and settings

---
+++ src/Ads/AjaxCrud/views/partials/modals/delete_modal.blade.php
@@ -0,0 +1,19 @@
+<div class="md-modal colored-header danger md-effect-10" id="delete-modal" style="perspective: none;">
+    <div class="md-content">
+        <div class="modal-header">
+            <h3><span class="header-text"></span></h3>
+            <button type="button" class="close md-close" data-dismiss="modal" aria-hidden="true">×</button>
+        </div>
+        <div class="modal-body">
+            <div class="text-center">
+                <div class="i-circle danger"><i class="fa fa-times"></i></div>
+                <h4><span class="body-text"></span></h4>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-danger btn-flat md-close ads-delete" data-id="0" data-dismiss="modal">Remove</button>
+            <button type="button" class="btn btn-default btn-flat md-close ads-delete-cancel" data-dismiss="modal">Cancel</button>
+        </div>
+    </div>
+</div>
+<div class="md-overlay"></div>
